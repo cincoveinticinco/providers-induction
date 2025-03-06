54e6b639e1ef98a890c918237f94c55e6ff1f868
sebascifuentess <j.cifuentes@cincoveinticinco.com>
2025-03-06 14:25:38 -0500

    feat: add baseform to sst and compliance

--- src/app/pages/private/compliance-form/compliance-form.component.ts
+++ src/app/pages/private/compliance-form/compliance-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { BaseForm } from '../../../bases/form.base';
 
 @Component({
   selector: 'app-compliance-form',
@@ -9,22 +10,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './compliance-form.component.html',
   styleUrl: './compliance-form.component.scss'
 })
-export class ComplianceFormComponent {
-
-    form: FormGroup;
+export class ComplianceFormComponent extends BaseForm {
   
     constructor() {
-      this.form = new FormGroup({
+      const form = new FormGroup({
         conflicts: new FormControl('', Validators.required),
         asset_laundering: new FormControl('', Validators.required),
-        extortion: new FormControl('', [Validators.required, Validators.email]),
+        extortion: new FormControl('', Validators.required),
         bribery: new FormControl('', Validators.required),
         data_processing: new FormControl('', Validators.required)
-      })
+      });
+      super(form);
     }
   
     submit() {
-      console.log(this.form.getRawValue());
+      console.log(this.parentForm.getRawValue());
+      if (this.parentForm.invalid) {
+        this.parentForm.markAllAsTouched();
+        return;
+      }
     }
 
 }
--- src/app/pages/private/sst-form/sst-form.component.ts
+++ src/app/pages/private/sst-form/sst-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { BaseForm } from '../../../bases/form.base';
 
 @Component({
   selector: 'app-sst-form',
@@ -9,26 +10,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './sst-form.component.html',
   styleUrl: './sst-form.component.scss'
 })
-export class SstFormComponent {
-
-    form: FormGroup;
+export class SstFormComponent extends BaseForm {
   
     constructor() {
-      this.form = new FormGroup({
+      const form = new FormGroup({
         security_general: new FormControl('', Validators.required),
         sst_requirementes: new FormControl('', Validators.required),
-        emergency_prevention: new FormControl('', [Validators.required, Validators.email]),
+        emergency_prevention: new FormControl('', Validators.required),
         prevention_accidents: new FormControl('', Validators.required),
         no_work_accident: new FormControl('', Validators.required),
         work_dangers: new FormControl('', Validators.required),
         unsafe_act: new FormControl('', Validators.required),
         sst_responsabilities: new FormControl('', Validators.required),
         when_is_work_accident: new FormControl('', Validators.required),
-      })
+      });
+      super(form);
     }
   
     submit() {
-      console.log(this.form.getRawValue());
+      console.log(this.parentForm.getRawValue());
+      if (this.parentForm.invalid) {
+        this.parentForm.markAllAsTouched();
+        return;
+      }
     }
 
 }
